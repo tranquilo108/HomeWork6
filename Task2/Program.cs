06ca703cba43ec57e2aa146f6bb0c9869c952f23
tranquilo108 <118503079+tranquilo108@users.noreply.github.com>
2022-12-18 01:36:31 +0700

    1

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -1,4 +1,9 @@
-﻿static void NewMethod1()
+//Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+//0, 7, 8, -2, -2 -> 2
+//1, -7, 567, 89, 223-> 3
+
+
+static void NewMethod1()
 {
     Console.WriteLine("Введите число: ");
     int a = Convert.ToInt32(Console.ReadLine());
