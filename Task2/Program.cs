3c43228352d74d51a059ab06e070aabd3ddc7503
tranquilo108 <sanbonzakurakogyoshi@gmail.com>
2022-12-18 01:32:38 +0700

    Initial commit

---
+++ Task2/Program.cs
@@ -0,0 +1,16 @@
+﻿static void NewMethod1()
+{
+    Console.WriteLine("Введите число: ");
+    int a = Convert.ToInt32(Console.ReadLine());
+    int count = 0;
+    for (int i = 0; i < a; i++)
+    {
+        int z = Convert.ToInt32(Console.ReadLine());
+        if (z > 0)
+        {
+            count++;
+        }
+        Console.WriteLine(count);
+    }
+}
+NewMethod1();
